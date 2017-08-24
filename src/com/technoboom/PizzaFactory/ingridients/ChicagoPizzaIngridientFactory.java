19bd7d31c8f4724543a3bd91898ef24edcb98fc8
dimi3ryi <feschenko.dmitryi@gmail.com>
2017-08-24 21:54:40 +0300

    init inversion of control for pizza store application

---
+++ src/com/technoboom/PizzaFactory/ingridients/ChicagoPizzaIngridientFactory.java
@@ -0,0 +1,44 @@
+package com.technoboom.PizzaFactory.ingridients;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Date: 8/24/17
+ * Time: 9:51 PM
+ * Project: patterns-learn
+ * Package: com.technoboom.PizzaFactory.ingridients
+ *
+ * @author dmitryi
+ * @version 1.0
+ * @since 1.0
+ */
+public class ChicagoPizzaIngridientFactory implements PizzaIngredientFactory {
+    @Override
+    public Dough createDough() {
+        return new ThichCrustDough();
+    }
+
+    @Override
+    public Cheese createCheese() {
+        return new Mozzarella();
+    }
+
+    @Override
+    public Pepperoni createPepperoni() {
+        return new SlicedPepperoni();
+    }
+
+    @Override
+    public Sauce createSauce() {
+        return new PlumTomatoSauce();
+    }
+
+    @Override
+    public Clams createClam() {
+        return new FrozenClams();
+    }
+
+    @Override
+    public Veggies[] createVeggies() {
+        return { new BlackOlives(), new Spinach(), new EggPlant() };
+    }
+}
---
+++ src/com/technoboom/PizzaFactory/ingridients/PizzaIngredientFactory.java
@@ -0,0 +1,21 @@
+package com.technoboom.PizzaFactory.ingridients;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Date: 8/24/17
+ * Time: 9:45 PM
+ * Project: patterns-learn
+ * Package: com.technoboom.PizzaFactory
+ *
+ * @author dmitryi
+ * @version 1.0
+ * @since 1.0
+ */
+public interface PizzaIngredientFactory {
+    public Dough createDough();
+    public Sauce createSauce();
+    public Cheese createCheese();
+    public Veggies[] createVeggies();
+    public Pepperoni createPepperoni();
+    public Clams createClam();
+}
